2596c4f694cae6a5a648916c0b8d74e99a48eeb8
franciairma <francia.irma@gmail.com>
2017-09-19 14:35:43 +0400

    new update site manitra

---
+++ js/scripting.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    $('#myTabs a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+
+    $('ul.tabs li').click(function () {
+        var tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#" + tab_id).addClass('current');
+    })
+
+    
+    $(function () {
+        var $gallery = $('.gallery a').simpleLightbox();
+
+        $gallery.on('show.simplelightbox', function () {
+                console.log('Requested for showing');
+            })
+            .on('shown.simplelightbox', function () {
+                console.log('Shown');
+            })
+            .on('close.simplelightbox', function () {
+                console.log('Requested for closing');
+            })
+            .on('closed.simplelightbox', function () {
+                console.log('Closed');
+            })
+            .on('change.simplelightbox', function () {
+                console.log('Requested for change');
+            })
+            .on('next.simplelightbox', function () {
+                console.log('Requested for next');
+            })
+            .on('prev.simplelightbox', function () {
+                console.log('Requested for prev');
+            })
+            .on('nextImageLoaded.simplelightbox', function () {
+                console.log('Next image loaded');
+            })
+            .on('prevImageLoaded.simplelightbox', function () {
+                console.log('Prev image loaded');
+            })
+            .on('changed.simplelightbox', function () {
+                console.log('Image changed');
+            })
+            .on('nextDone.simplelightbox', function () {
+                console.log('Image changed to next');
+            })
+            .on('prevDone.simplelightbox', function () {
+                console.log('Image changed to prev');
+            })
+            .on('error.simplelightbox', function (e) {
+                console.log('No image found, go to the next/prev');
+                console.log(e);
+            });
+    });
+});
